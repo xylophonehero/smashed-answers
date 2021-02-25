f6484ee2081bd42f5800b44ab98c9c3b04f231f7
XylophoneHero <nick97000@gmail.com>
2021-02-25 12:50:53 +0000

    integrate with vercel

--- pages/quiz.js
+++ pages/quiz.js
@@ -15,7 +15,7 @@ export default QuizPage;
 export async function getServerSideProps({ query })
 {
   console.log(query.new)
-  const questions_res = await fetch(`${API_URL}/questions/?_limit=${query.limit}&_sort=${query.sort}`)
+  const questions_res = await fetch(`${API_URL}/questions?_limit=${query.limit}&_sort=${query.sort}`)
   const questions = await questions_res.json()
 
   return {
