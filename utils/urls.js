842ef6480c0ea7a9c2f58bb77a62d8b7a568d0dc
XylophoneHero <nick97000@gmail.com>
2021-02-25 13:00:39 +0000

    updated url access

--- utils/urls.js
+++ utils/urls.js
@@ -1,3 +1,3 @@
-export const API_URL = process.env.STRAPI_API_URL || "http://localhost:1337"
+export const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:1337"
 
 export const CORS_PROXY_URL = "https://evening-eyrie-12151.herokuapp.com"
